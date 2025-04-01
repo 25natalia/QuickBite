a4577f261f4f30584f49629a4955e0627e0f1433
25natalia <25natalia09garcia@gmail.com>
2025-04-01 11:23:52 -0500

    fix git: arreglar error

---
+++ quick-bite/src/data/categories.js
@@ -0,0 +1,18 @@
+export const categories = [
+	{
+		id: 1,
+		name: 'Breakfast',
+	},
+	{
+		id: 2,
+		name: 'Lunch',
+	},
+	{
+		id: 3,
+		name: 'Dinner',
+	},
+	{
+		id: 4,
+		name: 'Snack',
+	},
+];
