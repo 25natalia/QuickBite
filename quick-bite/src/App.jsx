34e44221a370ed11b7a0383c27ac9784b14ccfb0
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2025-04-03 17:41:20 -0500

    feat: navegación en dos primeras pages

--- quick-bite/src/App.jsx
+++ quick-bite/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/home';
+import Welcome from './pages/Welcome/welcome';
 
 function App() {
-
-
-  return (
-    <>
-<h1>Hola</h1>
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Welcome />} />
+				<Route path='/home' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
